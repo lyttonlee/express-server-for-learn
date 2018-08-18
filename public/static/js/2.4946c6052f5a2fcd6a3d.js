60e8a7f8723d27b50ce8f2c3be5b2b7cb7ffdf93
lyttonlee <lzr3278@163.com>
2018-08-18 15:26:43 +0800

    server

---
+++ public/static/js/2.4946c6052f5a2fcd6a3d.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{JxsG:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r=n("bDIP"),a=n("VU/8"),u=a(null,r.a,!1,null,null,null);t.default=u.exports},bDIP:function(e,t,n){"use strict";var r=function(){var e=this,t=e.$createElement;return(e._self._c||t)("mange-menu",{attrs:{pathName:"批发管理"}})},a=[],u={render:r,staticRenderFns:a};t.a=u}});
+//# sourceMappingURL=2.4946c6052f5a2fcd6a3d.js.map
